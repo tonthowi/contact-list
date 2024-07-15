168cadfda9fa6170cfd10926086d6d1616787f22
Tonthowi Al Ahyar <tonthowi@gmail.com>
2024-07-15 11:50:11 +0700

    I screw'd

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import Forms from './Forms'
 import ContactList from './ContactList'
 
+import peopleService from './services/peoples.js'
+
 
 const App = () => {
-  const [peoples, setPeoples] = useState([
-    // { id: '1', name: 'John Doe', email: 'john.doe@example.com', phoneNumber: '1234567890' },
-    // { id: '2', name: 'Jane Doe', email: 'jane.doe@example.com', phoneNumber: '9876543210' },
-    // { id: '3', name: 'Alice Doe', email: 'alice.doe@example.com', phoneNumber: '0987654321' },
-    // { id: '4', name: 'Bob Doe', email: 'bob.doe@example.com', phoneNumber: '2345678901' },
-    // { id: '5', name: 'Charlie Doe', email: 'charlie.doe@example.com', phoneNumber: '3456789012' },
-    // { id: '6', name: 'David Doe', email: 'david.doe@example.com', phoneNumber: '4567890123' },
-    // { id: '7', name: 'Emily Doe', email: 'emily.doe@example.com', phoneNumber: '5678901234' },
-    // { id: '8', name: 'Frank Doe', email: 'frank.doe@example.com', phoneNumber: '6789012345' },
-    // { id: '9', name: 'Grace Doe', email: 'grace.doe,example.com', phoneNumber: '789012345' },
-  ])
+  const [peoples, setPeoples] = useState([])
   const [newName, setNewName] = useState('')
   const [newEmail, setNewEmail] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showSearch, setShowSearch] = useState('')
   const [alert, setAlert] = useState({message: [], show: false})
 
+  // Handle API call to fetch contacts from the server and update the state with the response data 
+  useEffect(() => {
+    peopleService
+    .getAll()
+    .then(initialPeoples => {
+      setPeoples(initialPeoples)
+      console.log('Initialize peoples from the json',initialPeoples)
+    })
+  },[])
+
+  // Reset form fields to default values for name, email, and phone number
   const resetFormFields = () => {
     setNewName('')
     setNewEmail('')
     setNewNumber('')
   }
-
+  // Add new contact to the list of contacts and display alert if duplicate contact found
   const addPerson = (event) => {
     event.preventDefault()
 
+    // Check if any of the form fields are empty. Display alert if any field is empty.
     if (newName === '' || newEmail === '' || newNumber === '') {
       setAlert({ messages: ['Please fill in all fields'], show: true })
       return
     }
-  
+    
+    // Create new contact object with unique ID, current timestamp, and form data.
     const formData = {
       id: uuidv4(),
       name: newName,
@@ -45,30 +50,44 @@ const App = () => {
       timestamp: Date.now()
     }
 
-    const duplicateMessages = []
-    if (peoples.find(person => person.name === formData.name)) {
-      duplicateMessages.push(`${formData.name} already exists in the contact list`)
-    }
-    if (peoples.find(person => person.email === formData.email)) {
-      duplicateMessages.push(`${formData.email} already exists in the contact list`)
-    }
-    if (peoples.find(person => person.phoneNumber === formData.phoneNumber)) {
-      duplicateMessages.push(`${formData.phoneNumber} already exists in the contact list`)
-    }
+    // Check for duplicate contacts in the list. Display alert if duplicates found.
+    if (formData) {
+      const duplicateMessages = []
+      if (peoples.find(person => person.name === formData.name)) {
+        duplicateMessages.push(`${formData.name} already exists in the contact list`)
+      }
+      else if (peoples.find(person => person.email === formData.email)) {
+        duplicateMessages.push(`${formData.email} already exists in the contact list`)
+      }
+      else if (peoples.find(person => person.phoneNumber === formData.phoneNumber)) {
+        duplicateMessages.push(`${formData.phoneNumber} already exists in the contact list`)
+      }
+      // Display alert if duplicate contact found in the list. Otherwise, add new contact to the list.
+      else if (duplicateMessages.length > 0) {
+        setAlert({ messages: duplicateMessages, show: true })
+      } else {
+          peopleService
+          .create(formData)
+          .then(returnedData => {
+            setPeoples([returnedData, ...peoples])
+          })
+          .then(() => {
+            resetFormFields() // Reset form fields
+            setAlert({ messages: [], show: false }) // Hide alert
+          })
+      }}
+  }
 
-    // Display alert if duplicate contact found in the list. Otherwise, add new contact to the list.
-    if (duplicateMessages.length > 0) {
-      setAlert({ messages: duplicateMessages, show: true })
-    } else {
-      setPeoples([formData, ...peoples]) // Add new contact to the list
-      resetFormFields() // Reset form fields
-      setAlert({ messages: [], show: false }) // Hide alert
-    }}
 
   // Sort contacts alphabetically A to Z
   const sortAlphabetically = () => {
-      const sortedAtoZ = [...peoples].sort( (a, b) => a.name.localeCompare(b.name) )
-      setPeoples(sortedAtoZ)
+    const sortedAtoZ = [...peoples].sort((a, b) => {
+      if (!a.name || !b.name) {
+        return 0
+      }
+      return a.name.localeCompare(b.name)
+    })
+    setPeoples(sortedAtoZ)
   }
   // Sort contacts by the most recently added
   const sortNewlyAdded = () => {
@@ -76,25 +95,30 @@ const App = () => {
     setPeoples(sortedNewlyAdded)
   }
 
+  // Filter contacts based on search keyword
+  const personToShow = showSearch === ''
+     ? peoples
+     : peoples.filter(person => person.name.toLowerCase().includes(showSearch.toLowerCase()))
+
+  // Handle search input change
   const handleSearchChange = (event) => {
     event.preventDefault()
     setShowSearch(event.target.value)
   }
 
-  const personToShow = showSearch === ''
-     ? peoples
-     : peoples.filter(person => person.name.toLowerCase().includes(showSearch.toLowerCase()))
-
+  // Handle name input change
   const handleNameChange = (event) => {
     event.preventDefault()
     setNewName(event.target.value)
   }
 
+  // Handle email input change
   const handleEmailChange = (event) => {
     event.preventDefault()
     setNewEmail(event.target.value)
   }
 
+  // Handle phone number input change
   const handleNumChange = (event) => {
     event.preventDefault()
     setNewNumber(event.target.value)
@@ -102,7 +126,6 @@ const App = () => {
 
   return (
     <div className='min-h-screen max-w-full space-y-6 p-4 sm:p-4 lg:p-4'>
-
       <div className='flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4'>
 
         <Forms
@@ -127,8 +150,7 @@ const App = () => {
           }}
         />
 
-      </div>
-      
+      </div>     
     </div>
   )
 }
---
+++ src/services/peoples.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3000/peoples' // Replace 'http://localhost:3000/peoples' with the actual API endpoint.
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = () => {
+    const request = axios.post(baseUrl)
+    return request.then(response => response.data)
+}
+
+const update = () => {
+    const request = axios.put(baseUrl)
+    return request.then(response => response.data)
+}
+
+const remove = () => {
+    const request = axios.delete(baseUrl)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, update, remove}
